fef33a497e9653567bea265989ae6dc6dc7d554d
emoilenne <emoilenne@gmail.com>
2017-07-24 15:51:25 -0700

    Added handling of errors

--- predict.py
+++ predict.py
@@ -1,6 +1,9 @@
 from trainingAgent import TrainingAgent
 
-agent = TrainingAgent(height=20, width=20, minSteps=1, maxSteps=5)
+try:
+    agent = TrainingAgent(height=20, width=20, minSteps=1, maxSteps=5)
 
-agent.loadModels()
-agent.predict('csv/test.csv', 'csv/submission.csv')
+    agent.loadModels()
+    agent.predict('csv/test.csv', 'csv/submission.csv')
+except Exception as e:
+    print "An error occured: " + str(e)
--- train.py
+++ train.py
@@ -3,13 +3,16 @@ from sys import argv
 
 MAPS = 100
 
-agent = TrainingAgent(height=20, width=20, minSteps=1, maxSteps=5)
+try:
+    agent = TrainingAgent(height=20, width=20, minSteps=1, maxSteps=5)
 
-if (len(argv) != 2):
-    print "usage: ./%s {filename.csv or 'generate'}"
-    exit(1)
-if argv[1] == 'generate':
-    agent.generateAndTrain(MAPS)
-else:
-    agent.train(argv[1])
-agent.saveModels()
+    if len(argv) == 0 or len(argv) > 2:
+        print "usage: python %s [generate]"
+        exit(1)
+    if len(argv) == 2 and argv[1] == 'generate':
+        agent.generateAndTrain(MAPS)
+    else:
+        agent.train('csv/train.csv')
+    agent.saveModels()
+except Exception as e:
+    print "An error occured: " + str(e)
